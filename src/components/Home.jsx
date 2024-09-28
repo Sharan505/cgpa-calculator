36a16dd4ba9d6f59a3dd0b40bab8eb2adbcd1abf
Sharan505 <sharan27505@gmail.com>
2024-09-28 11:07:19 +0530

    changes 2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -27,7 +27,25 @@ function Home(){
                         <Link to="/sem-eight" className="btn btn-outline-primary">8th Semester</Link>
                     </div>
                     <div className="col-md-3 col-sm-0"></div>
-                </div>
+                </div><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+                <hr className="mx-5 text-secondary"/><br />
+                <footer className="row">
+                    <p className="text-end pe-5 col-6" id="sign">Developed by Sharan M <br />IT 3rd year</p>
+                    <div className="col-6 d-flex justify-content-start gap-4 icons">
+                        <a id="linkedin" href="www.linkedin.com/in/sharan-m-18274932b" target="_blank" rel="noopener noreferrer">
+                            <ion-icon name="logo-linkedin"></ion-icon>
+                        </a>
+                        <a id="whatsapp" href="https://wa.me/917540008788" target="_blank" rel="noopener noreferrer">
+                            <ion-icon name="logo-whatsapp"></ion-icon>
+                        </a>
+                        <a id="github" href="https://github.com/Sharan505" target="_blank" rel="noopener noreferrer">
+                            <ion-icon name="logo-github"></ion-icon>
+                        </a>
+                        <a id="mail" href="mailto:sharan27505@gmail.com" target="_blank" rel="noopener noreferrer">
+                            <ion-icon name="mail-outline"></ion-icon>
+                        </a>
+                    </div>
+                </footer>
         </section>
     )
 }
